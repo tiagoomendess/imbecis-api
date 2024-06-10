aad90a8421483e1d2f096c78b39132625ec744a3
Tiago Mendes <tiagoomendess@gmail.com>
2024-06-10 15:04:51 +0100

    debug info for votes, only requiring 2 votes

--- src/useCases/vote.ts
+++ src/useCases/vote.ts
@@ -95,10 +95,13 @@ const runTransitionVerification = async (report: Report, votes: ReportVote[]) =>
     const imbecileVotes = voteResults[RESULTS.IMBECILE] ?? 0
     const notSureVotes = voteResults[RESULTS.NOT_SURE] ?? 0
 
+    console.log(`Votes array length ${votes.length}, imbecileVotes ${imbecileVotes}, notSureVotes ${notSureVotes}, totalVotes ${totalVotes}`)
+
     // If there are 3 or more cotes with result = imbecile and all the votes agree on the same plate number, confirm the report
-    if (imbecileVotes >= 3 && imbecileVotes > totalVotes * 0.66) {
+    if (imbecileVotes >= 4 && imbecileVotes > totalVotes * 0.66) {
         for (const plateNumber in plateNumbers) {
-            if (plateNumbers[plateNumber] >= 3 && plateNumber != "") {
+            if (plateNumbers[plateNumber] >= 4 && plateNumber != "") {
+                Logger.info(`Plate ${plateNumber} has ${plateNumbers[plateNumber]} votes and will be confirmed as imbecile`)
                 const plate = await getOrCreatePlate(plateNumber, mostVotedCountry)
                 report.plateId = plate._id
                 report.status = STATUS.NOTIFY
