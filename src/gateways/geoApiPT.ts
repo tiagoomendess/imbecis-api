c81858dfe07d6d6a633347d10a21c3e151a65e3d
Tiago Mendes <tiagoomendess@gmail.com>
2025-11-25 21:33:45 +0000

    cenas

--- src/gateways/geoApiPT.ts
+++ src/gateways/geoApiPT.ts
@@ -3,8 +3,37 @@ import config from '../config'
 
 const BASE_URL = 'https://json.geoapi.pt'
 
-export const getMunicipalityByCoords = async (latitude: number, longitude: number) : Promise<string | null> => {
+const proxyList = [
+    "138.68.60.8:80",
+    "no_proxy",
+    "8.221.141.88:31433",
+    "152.230.215.123:80",
+    "8.213.156.191:8008",
+    "203.74.125.18:8888",
+    "5.161.103.41:88",
+    "no_proxy",
+    "35.209.198.222:80",
+    "133.232.93.66:80",
+    "89.116.34.113:80",
+    "139.59.1.14:80",
+    "138.68.235.51:80",
+    "no_proxy"
+]
+
+export const getMunicipalityByCoords = async (latitude: number, longitude: number): Promise<string | null> => {
     try {
+
+        // Select 1 random proxy from the list
+        const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)]
+
+        if (randomProxy !== "no_proxy") {
+            // set axios to use the proxy
+            axios.defaults.proxy = {
+                host: randomProxy.split(':')[0],
+                port: parseInt(randomProxy.split(':')[1])
+            }
+        }
+
         const response = await axios.get(`${BASE_URL}/gps?lat=${latitude}&lon=${longitude}&ext-apis=false`)
         if (response.status !== 200) {
             return null
@@ -14,6 +43,9 @@ export const getMunicipalityByCoords = async (latitude: number, longitude: numbe
     } catch (error) {
         console.error(`Could not get municipality for coordinates ${latitude}, ${longitude} from GeoApiPT: ${error}`)
         return null
+    } finally {
+        // Reset axios to not use any proxy
+        axios.defaults.proxy = false
     }
 }
 
@@ -31,7 +63,7 @@ export interface GeoApiPTResponse {
     CP: string
 }
 
-export const getFullInfoByCoords = async (latitude: number, longitude: number) : Promise<GeoApiPTResponse | null> => {
+export const getFullInfoByCoords = async (latitude: number, longitude: number): Promise<GeoApiPTResponse | null> => {
     try {
         // wait 1 second to avoid hitting rate limits
         await new Promise(resolve => setTimeout(resolve, 1000))
