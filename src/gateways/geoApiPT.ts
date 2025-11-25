ba9d35cdfe9b144908e02407e86e56eba17a800a
Tiago Mendes <tiagoomendess@gmail.com>
2025-11-25 21:23:17 +0000

    removed key for geo api

--- src/gateways/geoApiPT.ts
+++ src/gateways/geoApiPT.ts
@@ -5,7 +5,7 @@ const BASE_URL = 'https://json.geoapi.pt'
 
 export const getMunicipalityByCoords = async (latitude: number, longitude: number) : Promise<string | null> => {
     try {
-        const response = await axios.get(`${BASE_URL}/gps?lat=${latitude}&lon=${longitude}&ext-apis=false&key=${config.geoApiPT.key}`)
+        const response = await axios.get(`${BASE_URL}/gps?lat=${latitude}&lon=${longitude}&ext-apis=false`)
         if (response.status !== 200) {
             return null
         }
