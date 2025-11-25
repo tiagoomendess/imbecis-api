a73049765b973a5d5dc29add99e6562817a932b2
Tiago Mendes <tiagoomendess@gmail.com>
2025-11-25 21:47:46 +0000

    asdlaskhjd

--- src/gateways/geoApiPT.ts
+++ src/gateways/geoApiPT.ts
@@ -4,37 +4,9 @@ import Logger from '../utils/logger'
 
 const BASE_URL = 'https://json.geoapi.pt'
 
-const proxyList = [
-    "138.68.60.8:80",
-    "no_proxy",
-    "8.221.141.88:31433",
-    "152.230.215.123:80",
-    "8.213.156.191:8008",
-    "203.74.125.18:8888",
-    "5.161.103.41:88",
-    "no_proxy",
-    "35.209.198.222:80",
-    "133.232.93.66:80",
-    "89.116.34.113:80",
-    "139.59.1.14:80",
-    "138.68.235.51:80",
-    "no_proxy"
-]
-
 export const getMunicipalityByCoords = async (latitude: number, longitude: number): Promise<string | null> => {
+    Logger.info(`API Key: ${config.geoApiPT.key}`)
     try {
-        // Select 1 random proxy from the list
-        const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)]
-        Logger.info(`Using proxy ${randomProxy} for GeoApiPT request`)
-
-        if (randomProxy !== "no_proxy") {
-            // set axios to use the proxy
-            axios.defaults.proxy = {
-                host: randomProxy.split(':')[0],
-                port: parseInt(randomProxy.split(':')[1])
-            }
-        }
-
         // wait 1 second to avoid hitting rate limits
         await new Promise(resolve => setTimeout(resolve, 1000))
 
@@ -45,7 +17,7 @@ export const getMunicipalityByCoords = async (latitude: number, longitude: numbe
 
         return response.data?.concelho || null
     } catch (error) {
-        console.error(`Could not get municipality for coordinates ${latitude}, ${longitude} from GeoApiPT: ${error}`)
+        Logger.error(`Could not get municipality for coordinates ${latitude}, ${longitude} from GeoApiPT: ${error}`)
         return null
     } finally {
         // Reset axios to not use any proxy
@@ -68,19 +40,8 @@ export interface GeoApiPTResponse {
 }
 
 export const getFullInfoByCoords = async (latitude: number, longitude: number): Promise<GeoApiPTResponse | null> => {
+    Logger.info(`API Key: ${config.geoApiPT.key}`)
     try {
-        // Select 1 random proxy from the list
-        const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)]
-        Logger.info(`Using proxy ${randomProxy} for GeoApiPT request`)
-
-        if (randomProxy !== "no_proxy") {
-            // set axios to use the proxy
-            axios.defaults.proxy = {
-                host: randomProxy.split(':')[0],
-                port: parseInt(randomProxy.split(':')[1])
-            }
-        }
-
         // wait 1 second to avoid hitting rate limits
         await new Promise(resolve => setTimeout(resolve, 1000))
 
@@ -95,7 +56,7 @@ export const getFullInfoByCoords = async (latitude: number, longitude: number): 
 
         return response.data as GeoApiPTResponse
     } catch (error) {
-        console.error(`Could not get full info for coordinates ${latitude}, ${longitude} from GeoApiPT: ${error}`)
+        Logger.error(`Could not get full info for coordinates ${latitude}, ${longitude} from GeoApiPT: ${error}`)
         return null
     }
 }
